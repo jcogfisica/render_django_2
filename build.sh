5207bf428eb25f78cec19629d0a3d53c6b74cc1f
jcogfisica <jcogfisica@yahoo.com.br>
2025-08-09 10:10:52 -0300

    Projeto Django_2_final_19

--- build.sh
+++ build.sh
@@ -10,3 +10,11 @@ python manage.py collectstatic --noinput
 
 # Carrega os dados do arquivo backup.json
 python manage.py loaddata backup.json
+
+# Cria superusuário automaticamente, se não existir
+# O código a seguir faz o seguinte:
+# User.objects.filter(username='jcogfisica').exists() retorna True se já existir um usuário admin.
+# or User.objects.create_superuser(...) só executa a criação se o retorno do .exists() for False.
+echo "from django.contrib.auth.models import User
+User.objects.create_superuser('jcogfisica', 'jcogfisica@yahoo.com.br', 'MON010deo010')"
+| python manage.py shell
