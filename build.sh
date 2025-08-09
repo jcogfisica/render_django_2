ffa292f74db228609638b4cc19c2615ec7faead6
jcogfisica <jcogfisica@yahoo.com.br>
2025-08-09 10:14:40 -0300

    Projeto Django_2_final_20

--- build.sh
+++ build.sh
@@ -15,6 +15,8 @@ python manage.py loaddata backup.json
 # O código a seguir faz o seguinte:
 # User.objects.filter(username='jcogfisica').exists() retorna True se já existir um usuário admin.
 # or User.objects.create_superuser(...) só executa a criação se o retorno do .exists() for False.
-echo "from django.contrib.auth.models import User
-User.objects.create_superuser('jcogfisica', 'jcogfisica@yahoo.com.br', 'MON010deo010')"
-| python manage.py shell
+python manage.py shell << EOF
+from django.contrib.auth.models import User
+if not User.objects.filter(username='jcogfisica').exists():
+    User.objects.create_superuser('jcogfisica', 'jcogfisica@yahoo.com.br', 'MON010deo010')
+EOF
